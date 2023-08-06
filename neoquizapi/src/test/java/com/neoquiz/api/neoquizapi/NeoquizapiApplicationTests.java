549c5ade2810a544ba4f1767be87e1074a94a853
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2023-08-06 16:08:20 -0400

    initial commit
    *

---
+++ neoquizapi/src/test/java/com/neoquiz/api/neoquizapi/NeoquizapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoquiz.api.neoquizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoquizapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
